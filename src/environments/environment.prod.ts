2c184bed448df7b3f11b365b6c6d1106e499147a
tangoalphatwo <taylorsold@hotmail.com>
2022-08-18 20:38:58 -0500

    Built and Deployed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'https://jd4ddd0ke8.execute-api.us-east-1.amazonaws.com'
 };
